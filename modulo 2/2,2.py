0ab6e95e8d85ccab6f5da724dc8f28a6785b0f2f
Kaique_vv <55851348+Kaiquevit@users.noreply.github.com>
2024-03-06 23:18:55 -0300

    2.2

---
+++ modulo 2/2,2.py
@@ -0,0 +1,40 @@
+'''
+    Transforme o seguinte algoritmo em código. Lembre-se das funções print e type que já conhecemos de forma geral.
+
+    v1=5
+    v2=2
+
+    print('tipo de variavel 1:', type(v1))
+    print('tipo de variavel 2:', type(v2))
+
+    soma= v1/v2
+    print('valor da variavel 3:', soma)
+    print('valor da variavel 3:', type(soma))'''
+
+'''txt= 'O resultado é: '
+var=10
+var2=3.5
+soma=var+var2
+print("Variável 'texto':", txt)
+print("Tipo de 'texto':", type(txt))
+
+print("Variável '2':", var)
+print("Tipo de 'variavel':", type(var))
+
+print("Variável '3':", var2)
+print("Tipo de 'variavel':", type(var2))'''
+
+#Dadas duas variáveis v1 = 10 e v2 = 20, utilize uma terceira variável para trocar os valores entre as duas variáveis. Ou seja, ao final v1 terá o valor de v2, e v2 o valor de v1. Você deve usar uma variável auxiliar de troca, não podendo atribuir os novos valores diretamente.
+'''v1=10
+v2=20
+v3=v1
+v1=v2
+v2=v3
+print("Novo valor de v1:", v1)
+print("Novo valor de v2:", v2)'''
+
+'''juros = 1.01
+saldo = 500.0
+saldo*=juros**3
+print("Após 3 meses meu novo saldo é", saldo)
+'''
